17e2e2d413267928cd55053baee0f6b7d9f98dd7
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-10-04 18:00:26 +0700

    up

---
+++ src/Provider/AuthBundleServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Taisiya\AuthBundle\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class AuthBundleServiceProvider implements ServiceProviderInterface
+{
+	public function register(Container $pimple)
+	{
+		// TODO: Implement register() method.
+	}
+}
