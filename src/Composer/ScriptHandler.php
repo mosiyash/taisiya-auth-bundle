b05375e6bd491b4efabc34e29d1f4056bbcc9d66
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-10-04 18:25:50 +0700

    up

---
+++ src/Composer/ScriptHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Taisiya\AuthBundle\Composer;
+
+use Taisiya\CoreBundle\Composer\ScriptHandler as CoreScriptHandler;
+
+defined('ROOT_DIR') || define('ROOT_DIR', dirname(dirname(__DIR__)));
+
+class ScriptHandler extends CoreScriptHandler {}
